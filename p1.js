f6221c5030ca2b2b50a8deb26fa738a6b3e28b0c
Aashuumittal <148664725+Aashuumittal@users.noreply.github.com>
2025-08-21 02:56:56 +0530

    Add files via upload

---
+++ p1.js
@@ -0,0 +1,23 @@
+const buttons = document.querySelectorAll('.button')
+const body = document.querySelector('body');
+buttons.forEach(function(button){
+    
+    button.addEventListener('click' , function(e){
+        console.log(e)
+        console.log(e.target)
+        if (e.target.id === 'Grey') {
+            body.style.backgroundColor = e.target.id;
+        }
+        
+        if (e.target.id === 'Red') {
+            body.style.backgroundColor = e.target.id;
+        }
+        if (e.target.id === 'Blue') {
+            body.style.backgroundColor = e.target.id;
+        }
+        if (e.target.id === 'Purple') {
+            body.style.backgroundColor = e.target.id;
+        }
+        
+    });
+});
